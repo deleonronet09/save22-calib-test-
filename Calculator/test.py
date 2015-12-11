70202c011d3e3c4ee611f14561a1d25f20f80382
deleonronet09 <deleonronet09@outlook.com>
2015-12-11 15:39:47 +0800

    Test cases for calculator.py

---
+++ Calculator/test.py
@@ -0,0 +1,21 @@
+import calculator
+import unittest
+
+class TestCalculator(unittest.TestCase):
+  def test_Add(self):
+    self.assertEqual(calculator.add(1,1),2)
+  def test_Subtract(self):
+  	self.assertEqual(calculator.subtract(15,10),5)
+  def test_Multiply(self):
+  	self.assertEqual(calculator.multiply(1,1),1)
+  def test_Divide(self):
+    self.assertEqual(calculator.divide(10,0))
+
+  def test_operator(self):
+  	self.assertEqual(calculator.add(13,11),24)
+  	self.assertEqual(calculator.subtract(15,-10),25)
+  	self.assertEqual(calculator.divide(16,2),8)
+  	self.assertEqual(calculator.multiply(7,-3),-21)
+
+if __name__ == "__main__":
+   unittest.main()
