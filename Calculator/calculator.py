83f0b094f481e64a8596bcecb482b75461d76e77
deleonronet09 <deleonronet09@outlook.com>
2015-12-14 11:06:44 +0800

    Finished error catchings for string inputs and zero division error in calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -11,7 +11,6 @@ def divide(input1,input2):
   return input1/input2
   
 def operator(num1,op,num2):
-<<<<<<< HEAD
   functions = {'+':add, '-':subtract, '*':multiply, '/':divide}
   func = functions.get(op, None)
   if num1 == None or num2 == None:
@@ -53,45 +52,16 @@ def output(num1,op,num2,ans):
     return '{} can not be {} by {}'.format(num1,op,num2)
   else:
     return '{} {} {} = {}'.format(num1,op,num2,ans)
-=======
-  if op == '+':
-    return add(num1,num2)
-  elif op == '-':
-    return subtract(num1,num2)
-  elif op == '*':
-    return multiply(num1,num2)
-  elif op == '/':
-    return divide(num1,num2)
-  else:
-    return None 
-    
-def input1():
-  return int(raw_input("Enter first number: "))
-  
-def input2():
-  return int(raw_input("Enter second number: "))
-
-def inputop():
-  return raw_input("Enter operation: ")
-  
-def output(num1,op,num2,ans):
-  return '{} {} {} = {}'.format(num1,op,num2,ans)
->>>>>>> b423a7cf35c64cce351780af90892118efc61cfc
 
 def main():
   num1 = input1()
   op = inputop()
   num2 = input2()
   ans = operator(num1,op,num2)
-<<<<<<< HEAD
+
   print(output(num1,op,num2,ans))
   
 if __name__ == "__main__":
     main()
-=======
-  print output(num1,op,num2,ans)
   
-main()
-  
-  
->>>>>>> b423a7cf35c64cce351780af90892118efc61cfc
+
